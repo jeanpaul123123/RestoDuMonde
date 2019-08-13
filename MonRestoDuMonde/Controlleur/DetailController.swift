eddf7dc5844de424f8bddf56398b5ff589d801a6
Jean-Paul <jeanpaul123@gmail.com>
2019-08-13 21:22:50 +0200

    FInalisation de l'application

--- MonRestoDuMonde/Controlleur/DetailController.swift
+++ MonRestoDuMonde/Controlleur/DetailController.swift
@@ -26,7 +26,31 @@ class DetailController: UIViewController {
     }
     // #clicmenu
     func miseEnPlace(menu: Menu){
-        print("Ca marche on a le menu : \(menu.pays)")
+        //print("Ca marche on a le menu : \(menu.pays)")
+        let imageDuMenu = UIImageView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 250))
+        imageDuMenu.image = menu.image
+        imageDuMenu.contentMode = .scaleAspectFill
+        imageDuMenu.clipsToBounds = true
+        scroll.addSubview(imageDuMenu)
+        var maxY = imageDuMenu.frame.maxY
+        
+        let nomDuMenu = UILabel(frame: CGRect(x: 0, y: maxY, width: view.frame.width, height: 100))
+        nomDuMenu.text = "Menu " + menu.pays
+        nomDuMenu.font = FONT_TITRE
+        nomDuMenu.textColor = .red
+        nomDuMenu.adjustsFontSizeToFitWidth = true
+        nomDuMenu.textAlignment = .center
+        scroll.addSubview(nomDuMenu)
+        maxY = nomDuMenu.frame.maxY
+        
+        for plat in menu.plats {
+            let vue = PlatView(frame: CGRect(x: 0, y: maxY, width: view.frame.width, height: 375))
+            vue.miseEnPlace(plat: plat)
+            scroll.addSubview(vue)
+            maxY = vue.frame.maxY
+        }
+        
+        scroll.contentSize = CGSize(width: view.frame.width, height: maxY + 50)
     }
 
 
